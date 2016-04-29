3fe341caa8ca5dab459ae985cc1d072843f9ef9f
Mongit <monserrat.jimenez2@gmail.com>
2016-04-29 08:33:26 -0500

    Se eliminaron comentarios

--- web/public/angular/test/mapFactorySpec.js
+++ web/public/angular/test/mapFactorySpec.js
@@ -3,38 +3,7 @@ describe('mapFactory test', function() {
     beforeEach(module('app'));
     
     beforeEach(function() {
-    /*    
-        windowMock = {
-            document:{
-                getElementById: function(element){
-                    return {element};
-                }
-            },
-            google : {
-                maps:{
-                    eventListener: undefined,
-                    MapTypeId: {
-                        ROADMAP: "road"
-                    },
-                    LatLng: function (lat, long){
-                        return {lat,long}
-                    },
-                    Map: function (document, mapOptions) {
-                        return {document, mapOptions};
-                    },
-                    event:{
-                        addListener: function (map, name, callback){
-                            windowMock.google.maps.eventListener={map,name,callback};
-                            return {map,name,callback}
-                        }
-                    },
-                    Marker: function(){
-                        
-                    }
-                }
-            }
-        }
-      */  module(function ($provide) {
+        module(function ($provide) {
             $provide.value('$window', windowMock);
         });
     });
